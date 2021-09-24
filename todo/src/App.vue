0fbe139643cc1e176c7982708a0522dec0a03781
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-09-24 04:00:22 -0300

    Add-Styles-Add-form-buttons-todoList

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,27 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <hello msg="Welcome to Your Vue.js App" />
-  <hello bemvindo="Essa pagina esta sendo usada por mim hehe" />
+  <div class id="app">
+    <div class="container grid-xs py-2">
+
+      <img class="img-responsive img-todolist" src="@/assets/todolist.svg" alt="img-todolist">
+      <h3 id="titulo">TodoList</h3>
+
+      <form @submit.prevent="addTodo(todo)">
+          <div class="input-group">
+            <input v-model="todo.description" type="text" class="form-input" placeholder="Novo todo">
+           
+            <button class="btn btn-primary">Adicionar</button>
+          </div>
+      </form>
+      
+      <div class="todo-list"> 
+         <todo v-for="t in todos" :key="t.id" :todo="t"/>
+
+      </div>
+    </div>
+  </div>
+   
 </template>
 
 <script>
-import hello from './components/bemVindo.vue'
+// import hello from './components/hello.vue'
+import Todo from "./components/Todo.vue";
 
 export default {
-  name: 'App',
-  components: {
-    hello
+  name: 'app',
+  components: { Todo },
+  data() {
+    return { todos: [
+
+    ], todo: { checked: false } };
+  },
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+      this.todo = { checked : false }
+    }
   }
-}
+};
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+
+.img-todolist {
+  max-width: 350px;
+  max-height: 150px;
+
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 70px;
+
+  margin-bottom: 20px;
 }
+
+h3#titulo {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.todo-list {
+  padding-top: 2rem;
+}
+
 </style>
